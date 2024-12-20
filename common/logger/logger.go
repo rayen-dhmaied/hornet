5e9a11c84502d9165bc55f94ad091afb423c6f4e
Rayen Dhmaied <rayen.dhmaied@horizon-tech.tn>
2024-12-20 13:59:17 +0100

    Implement reusable logging

---
+++ common/logger/logger.go
@@ -0,0 +1,26 @@
+package logger
+
+import (
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+var logger *zap.Logger
+
+func init() {
+	var err error
+	// Initialize the logger with development settings (you can change this to production or customize as needed)
+	logger, err = zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+}
+
+// WithContext adds the Gin context to the logger to enrich log entries with request info
+func WithContext(c *gin.Context) *zap.SugaredLogger {
+	return logger.Sugar().With(
+		zap.String("method", c.Request.Method),
+		zap.String("uri", c.Request.RequestURI),
+		zap.String("ip", c.ClientIP()),
+	)
+}
